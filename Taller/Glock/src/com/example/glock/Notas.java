f81f54290b622b615637da2558882238af9fbc8c
root <root@l30bravo>
2014-10-12 15:53:57 -0300

    Taller - Structura Nota - Sqlite Conexion

---
+++ Taller/Glock/src/com/example/glock/Notas.java
@@ -0,0 +1,30 @@
+package com.example.glock;
+
+public class Notas{
+	private String id, titulo, contenido;
+	
+	public String getId(){
+		return id;
+	}
+	
+	public String getTitulo(){
+		return titulo;
+	}
+	
+	public String getContenido(){
+		return contenido;
+	}
+	
+	public void setTitulo(String x){
+		this.titulo=x;
+	}
+	
+	public void setContenido(String x){
+		this.contenido=x;
+	}
+	
+	public void setNota(String titulo, String contenido){
+		this.titulo=titulo;
+		this.contenido=contenido;
+	}
+}
