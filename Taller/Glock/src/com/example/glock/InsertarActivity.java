d0901afa7bab60c551977cf9d67c8ab1097cb696
Leonardo Bravo Illanes <leonardbravoillanes@gmail.com>
2014-10-12 18:06:41 -0300

    Taller - Insertar

--- Taller/Glock/src/com/example/glock/InsertarActivity.java
+++ Taller/Glock/src/com/example/glock/InsertarActivity.java
@@ -6,18 +6,51 @@ import android.os.Bundle;
 //import android.view.MenuItem;
 //New
 import android.content.Intent;
+import android.app.Activity;
 import android.database.sqlite.SQLiteDatabase;
 import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
 
 public class InsertarActivity extends ActionBarActivity {
 
 	String[] datos; //Para recibir la información
 	TextView titulo, contenido, edo; //Para mostrar en pantalla
+
+	Button volver=(Button)findViewById(R.id.volver);
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_insertar);
+		
+		//--Boton Volver--//
+        volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent button_volver = new Intent (InsertarActivity.this, MainActivity.class);
+                startActivity(button_volver);
+            }
+        });
+		
+		//--Recibir parametro--//
+		titulo=(TextView)findViewById(R.id.mostrarTitulo);
+		edo=(TextView)findViewById(R.id.mostrarEdo);
+		
+		//Obtengo la información de la actividad anterior y se la asigno a la variable info
+		Intent info=getIntent();
+		datos=info.getStringArrayExtra(AgregarActivity.ACT_INFO);
+		
+		//Muestreo la información en la interfaz
+		titulo.setText(datos[0]);
+		contenido.setText(datos[0]);
+		
+		//Abrimos la Base de datos
+		Sqlite user_db=new Sqlite(this, "DBUsuario", null, 1);
+		SQLiteDatabase db=user_db.getWritableDatabase();
+		db.execSQL("INSERT INTO notas (titulo, contenido) VALUES ('"+datos[0]+"', '"+datos[1]+"')");
+		db.close();
 	}
 
 	/*
