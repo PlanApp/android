294e98f3538c0ee93c27bd2f6687f32db009e991
Leonardo Bravo <leonardbravoillanes@gmail.com>
2014-11-28 18:12:22 -0300

    Fix vista rota de panoramas

--- PlanApp/src/com/example/planapp/MainPanoramas.java
+++ PlanApp/src/com/example/planapp/MainPanoramas.java
@@ -1,74 +1,16 @@
 package com.example.planapp;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainPanoramas extends Activity {
 
-	 TextView text_id_usuario;
-	 TextView text_mail;
-	 TextView text_latitud;
-	 TextView text_longitud;
-	 TextView text_acompanante;
-	 
-	 String id_usuario;
-	 String mail;
-	 String dinero;
-	 String acompanante;
-	 String latitud;
-	 String longitud;
-	 String presicion;
-	 
-	LocationManager locManager;
-	LocationListener locListener;
-
-	 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_panoramas);
-		
-		//GEOLOCALIZACION
-		comenzarLocalizacion();
-		
-		if(latitud == "SinDatos"){
-			Log.v("MainGenPanorama", "esperando ..");
-			//this.sleep(1000);
-			//wait(1000);
-			 android.os.SystemClock.sleep(1000); 
-			Intent ir_a = new Intent (this, MainGenPanorama.class);
-			startActivity(ir_a);
-		}
-		
-		//OBTENER DATOS DE LA OTRA VISTA
-		Intent info=getIntent();
-		id_usuario=info.getExtras().getString("id");
-		mail=info.getExtras().getString("mail");
-		acompanante=info.getExtras().getString("acompanante");
-		latitud=info.getExtras().getString("latitud");
-		longitud=info.getExtras().getString("longitud");
-		dinero=info.getExtras().getString("dinero");
-		
-		
-		
-		text_id_usuario = (TextView)findViewById(R.id.text_id_valor);
-		text_latitud = (TextView)findViewById(R.id.text_latitud);
-		text_longitud = (TextView)findViewById(R.id.text_longitud);
-		
-		//SETEO DE VALORES EN LA VISTA
-		text_id_usuario.setText(id_usuario);
-		text_latitud.setText(latitud);
-		text_longitud.setText(longitud);
-		
 	}
 
 	@Override
@@ -89,54 +31,4 @@ public class MainPanoramas extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	 // --- GEOLOCALIZACION ---//
-	 void comenzarLocalizacion()
-	 {
-	    	//Obtenemos una referencia al LocationManager
-		 
-	    	locManager =(LocationManager)getSystemService(Context.LOCATION_SERVICE);
-	    	
-	    	//Obtenemos la �ltima posici�n conocida
-	    	Location loc =locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-	    	
-	    	//Mostramos la �ltima posici�n conocida
-	    	mostrarPosicion(loc);
-	    	
-	    	//Nos registramos para recibir actualizaciones de la posici�n
-	    	this.locListener = new LocationListener() {
-		    	public void onLocationChanged(Location location) {
-		    		mostrarPosicion(location);
-		    	}
-		    	public void onProviderDisabled(String provider){
-		    		Log.i("", "Provider OFF ");
-		    	}
-		    	public void onProviderEnabled(String provider){
-		    		Log.i("", "Provider ON ");
-		    	}
-		    	
-		    	
-		    	public void onStatusChanged(String provider, int status, Bundle extras){
-		    		Log.v("", "Provider Status: " + status);
-		    	}
-	    	};
-	    	
-	    	locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0, locListener);
-	    }
-	     
-	    private void mostrarPosicion(Location loc) {
-	    	if(loc != null){
-	    		Log.i("", String.valueOf(loc.getLatitude() + " - " + String.valueOf(loc.getLongitude())));
-	    		latitud=String.valueOf(loc.getLatitude());
-	    		longitud=String.valueOf(loc.getLongitude());
-	    		presicion=String.valueOf(loc.getAccuracy());
-	    	}
-	    	else
-	    	{
-	    		latitud="SinDatos";
-	    		longitud="SinDatos";
-	    		presicion="SinDatos";
-	    	}
-	    }
-	    //---FIN GEOLOCALIZACIÓN ---//
 }
